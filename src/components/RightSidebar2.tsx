d1cfd7832bc35e3e566b34cec8c6509167bd08fe
widura26 <widurategalrejo@gmail.com>
2025-10-20 19:14:52 +0700

    feat: add image in rightsidebar2 card

--- src/components/RightSidebar2.tsx
+++ src/components/RightSidebar2.tsx
@@ -5,14 +5,15 @@ const RightSidebar2 = () => {
     const { openModal } = useModal()
     return (
         <div className="bg-white pt-5 pr-4 pb-5 text-white">
-            <div className="rounded-2xl bg-black w-full h-full p-6 flex flex-col gap-6">
-                <div className="flex flex-col gap-1">
+            <div className="overflow-hidden rounded-2xl w-full h-full p-6 flex flex-col gap-6 bg-[url('/createnewjob.jpg')] bg-cover relative">
+                <div className="flex flex-col gap-1 z-10">
                     <p className='font-bold text-[18px]'>Recruit the best candidates</p>
                     <p className='font-bold text-sm'>Create jobs, invite, and hire with ease</p>
                 </div>
-                <div className="w-full">
+                <div className="w-full z-10">
                     <button type='button' onClick={openModal} className='bg-[#01959F] cursor-pointer w-full text-white font-bold text-base py-[6px] px-4 rounded-lg'>Create a new job</button>
                 </div>
+                <div className="absolute z-5 inset-0 bg-black via-transparent to-transparent opacity-[72%]"></div>
             </div>
         </div>
     );
